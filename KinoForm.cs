c3a93007b8bb4beceace99826e4e7ffa44cc1785
Opilane TTHK <opilane@tthk.ee>
2022-01-06 10:48:06 +0200

    Add project files.

---
+++ KinoForm.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MinuVorm
+{
+
+    public partial class KinoForm : Form
+    {
+        public KinoForm()
+        {
+            this.Height = 200;
+            this.Width = 200;
+            BackColor = Color.Black;
+            this.Text = "Menüü";
+        }
+
+        public class AutoClosingMessageBox
+        {
+            System.Threading.Timer _timeoutTimer;
+            string _caption;
+            AutoClosingMessageBox(string text, string caption, int timeout)
+            {
+                _caption = caption;
+                _timeoutTimer = new System.Threading.Timer(OnTimerElapsed,
+                    null, timeout, System.Threading.Timeout.Infinite);
+                using (_timeoutTimer)
+                    MessageBox.Show(text, caption);
+            }
+            public static void Show(string text, string caption, int timeout)
+            {
+                new AutoClosingMessageBox(text, caption, timeout);
+            }
+            void OnTimerElapsed(object state)
+            {
+                IntPtr mbWnd = FindWindow("#32770", _caption); // lpClassName is #32770 for MessageBox
+                if (mbWnd != IntPtr.Zero)
+                    SendMessage(mbWnd, WM_CLOSE, IntPtr.Zero, IntPtr.Zero);
+                _timeoutTimer.Dispose();
+            }
+            const int WM_CLOSE = 0x0010;
+            [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true)]
+            static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+            [System.Runtime.InteropServices.DllImport("user32.dll", CharSet = System.Runtime.InteropServices.CharSet.Auto)]
+            static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
+        }          //Класс для автоматического закрытия всплывающего окна при переходе между формами
+        private void but_Click(object sender, EventArgs e)
+        {
+            AutoClosingMessageBox.Show("Please Wait", "Treatment", 1000);    //Авто закрытие меседж бокса
+            KinoForm Start_form = new KinoForm();    //переходна вторую форму
+            Start_form.Show();
+            this.Hide();
+        }
+
+    }
+}
